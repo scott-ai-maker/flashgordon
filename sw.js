8f064e0df6c73733a61ecda0db9124476e3627c5
Scott Gordon <scott.gordon72@outlook.com>
2026-03-08 21:38:54 -0400

    Update color scheme

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'flash-gordon-v1';
+const CACHE = 'flash-gordon-v2';
 const ASSETS = [
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Share+Tech+Mono&family=Rajdhani:wght@400;600;700&display=swap'
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600;700;900&family=Exo+2:ital,wght@0,300;0,400;0,600;0,700;1,300&family=Share+Tech+Mono&display=swap'
 ];
 
 self.addEventListener('install', e => {
